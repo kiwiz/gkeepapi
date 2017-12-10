32a5f25ab87f6304e677fe12277088b075e5a46d
K <z@kwi.li>
2017-12-10 11:43:15 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.5',
+    version='0.9.6',
 
     description='An unofficial Google Keep API client',
 
