78aaae8b988b1cf616e3973f7f15d4c6d5e996cc
K <z@kwi.li>
2019-04-02 03:11:48 -0400

    Issue #61: Fix ParseException in NodeDrawingInfo

--- gkeepapi/node.py
+++ gkeepapi/node.py
@@ -1675,10 +1675,10 @@ class NodeDrawingInfo(Element):
         super(NodeDrawingInfo, self)._load(raw)
         self.drawing_id = raw['drawingId']
         self.snapshot.load(raw['snapshotData'])
-        self._snapshot_fingerprint = raw['snapshotFingerprint']
+        self._snapshot_fingerprint = raw['snapshotFingerprint'] if 'snapshotFingerprint' in raw else self._snapshot_fingerprint
         self._thumbnail_generated_time = NodeTimestamps.str_to_dt(raw['thumbnailGeneratedTime']) if 'thumbnailGeneratedTime' in raw else NodeTimestamps.int_to_dt(0)
         self._ink_hash = raw['inkHash'] if 'inkHash' in raw else ''
-        self._snapshot_proto_fprint = raw['snapshotProtoFprint']
+        self._snapshot_proto_fprint = raw['snapshotProtoFprint'] if 'snapshotProtoFprint' in raw else self._snapshot_proto_fprint
 
     def save(self, clean=True):
         ret = super(NodeDrawingInfo, self).save(clean)
