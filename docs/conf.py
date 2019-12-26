ec7721a8f36697231c0ba0c9b05db8cda49e6721
K <z@kwi.li>
2019-12-26 16:52:02 -0500

    Docs: switch load order

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 #
 import os
 import sys
-import gkeepapi
 sys.path.insert(0, os.path.abspath('..'))
+import gkeepapi
 
 
 # -- General configuration ------------------------------------------------
